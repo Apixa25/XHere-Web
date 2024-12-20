50187b1c299be760b35265f3d96b127347d72233
Steven Sills II <stevensills2@gmail.com>
2024-12-19 22:13:16 -0800

    Updated app.js routes locations

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
+const locationRoutes = require('./routes/locationRoutes');
 
 // Add the routes
 app.use('/api/user', userRoutes);
+app.use('/api/locations', locationRoutes);
 app.use('/uploads', express.static('uploads'));
 
 // ... rest of your app configuration ... 
