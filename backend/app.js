5933aea0b13a707a06f7ebcf50c30129185903db
Steven Sills II <stevensills2@gmail.com>
2024-12-19 22:33:34 -0800

    app.js route is still not matching front to back

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const locationRoutes = require('./routes/locationRoutes');
 
 // Add the routes
 app.use('/api/user', userRoutes);
-app.use('/api/locations', locationRoutes);
+app.use('/api/user/locations', locationRoutes);
 app.use('/uploads', express.static('uploads'));
 
 // ... rest of your app configuration ... 
