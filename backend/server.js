bf0bcc080c176922a8d556399716af30973b6a81
Steven Sills II <stevensills2@gmail.com>
2024-12-24 22:30:14 -0800

    Switched from bcrypt to bcryptjs

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const cors = require('cors');
